48f823b0c02cfa993342267bb4927b46c04e32f7
Tima <80810016+Tima99@users.noreply.github.com>
2023-09-21 14:10:18 +0530

    deployment on render

--- controllers/entryController/index.js
+++ controllers/entryController/index.js
@@ -1,17 +1,10 @@
-const {
-    OtpVerified,
-    EmailVerified,
-    ResetPasswordVerified,
-} = require("./OtpVerification");
 
 module.exports = {
     isEmailVerified: require("./EmailStatus"),
     AuthenticateUser: require("./AuthenticateUser"),
     ResendOtp: require("./ResendOtp"),
     ResetPassword: require("./ResetPassword"),
-    OtpVerified,
-    EmailVerified,
-    ResetPasswordVerified,
+    ...require("./OtpVerification"),
     RefreshToken: require('./RefreshToken'),
     LogoutController: require('./LogoutController'),
     GetUser: require('./GetUser')
